b690d618db923d2746533d05078ac9cfa236bad8
AndreiZaycev <Mikowftv30@mail.ru>
2022-04-16 15:45:58 +0300

    workflow done

---
+++ Workflow/TestWorkflow/WorkflowTest.fs
@@ -0,0 +1,47 @@
+module TestWorkflow
+
+open NUnit.Framework
+open Workflow
+open FsUnit
+
+[<Test>]
+let testRound3 () =
+    let rounding = RoundingBuilder(3)
+
+    let res =
+        rounding {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+
+    res |> should equal 0.048
+
+[<Test>]
+let testForCalculate3 () =
+    let calculate = CalculateBuilder()
+
+    let res =
+        calculate {
+            let! x = "1"
+            let! y = "2"
+            let z = x + y
+            return z
+        }
+
+    res |> should equal (Some 3)
+
+[<Test>]
+let incorrectInputTest () =
+    let calculate = CalculateBuilder()
+
+    let res =
+        calculate {
+            let! x = "1"
+            let! y = "Ъ"
+            let z = x + y 
+            return z
+        }
+
+    res |> should equal None
+    
---
+++ Workflow/Workflow/Library.fs
@@ -0,0 +1,13 @@
+﻿module Workflow
+
+type RoundingBuilder(accuracy : int) =
+    member this.Bind (x : float, f) = (x, accuracy) |> System.Math.Round |> f
+    member this.Return (x : float) = System.Math.Round (x, accuracy)
+    
+/// Workflow builder for operations with string integer numbers.
+type CalculateBuilder() =
+    member this.Bind (x : string, f) =
+        match System.Int32.TryParse x with
+        | true, number -> number |> f
+        | _ -> None 
+    member this.Return (x : int) = Some x
