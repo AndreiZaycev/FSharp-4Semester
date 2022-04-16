370c68470b51c1d09e64761a98b879be61fb3aba
AndreiZaycev <Mikowftv30@mail.ru>
2022-04-16 15:48:47 +0300

    add comments

--- Workflow/Workflow/Library.fs
+++ Workflow/Workflow/Library.fs
@@ -1,10 +1,11 @@
 ﻿module Workflow
 
+/// rounding build for operations with given round accuracy
 type RoundingBuilder(accuracy : int) =
     member this.Bind (x : float, f) = (x, accuracy) |> System.Math.Round |> f
     member this.Return (x : float) = System.Math.Round (x, accuracy)
     
-/// Workflow builder for operations with string integer numbers.
+/// workflow builder for operations with string integer numbers
 type CalculateBuilder() =
     member this.Bind (x : string, f) =
         match System.Int32.TryParse x with
