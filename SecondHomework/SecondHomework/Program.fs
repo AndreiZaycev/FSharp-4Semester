f62a25fc20e92be20f8a721caca72b944a243c72
AndreiZaycev <Mikowftv30@mail.ru>
2022-04-28 22:56:31 +0300

    fix comments

--- SecondHomework/SecondHomework/Program.fs
+++ SecondHomework/SecondHomework/Program.fs
@@ -1,11 +1,12 @@
 module functions
 
 let mapEven list =
-    (List.map (fun a -> (a + 1) % 2) list)
-    |> List.reduce (+)
+    list
+    |> List.map (fun a -> (abs a + 1) % 2)
+    |> List.fold (+) 0
 
-let foldEven (list: List<int>) =
-    List.fold (fun acc a -> acc + (a + 1) % 2) 0 list
+let foldEven list =
+    List.fold (fun acc a -> acc + (abs a + 1) % 2) 0 list
 
 let filterEven list =
     (List.filter (fun a -> a % 2 = 0) list)
@@ -37,10 +38,10 @@ let rec evaluateExpression tree =
     match tree with
     | Node (operation, left, right) ->
         match operation with
-        | Sum -> (+) (evaluateExpression left) (evaluateExpression right)
-        | Subtract -> (-) (evaluateExpression left) (evaluateExpression right)
-        | Divide -> (/) (evaluateExpression left) (evaluateExpression right)
-        | Multiply -> (*) (evaluateExpression left) (evaluateExpression right)
+        | Sum -> (evaluateExpression left) + (evaluateExpression right)
+        | Subtract -> (evaluateExpression left) - (evaluateExpression right)
+        | Divide -> (evaluateExpression left) / (evaluateExpression right)
+        | Multiply -> (evaluateExpression left) * (evaluateExpression right)
     | Leaf a -> a
 
 let primeGenerator =
@@ -61,3 +62,4 @@ let primeGenerator =
 
             counter <- counter + 1
     }
+ 
--- SecondHomework/SecondHomeworkTests/Testings.fs
+++ SecondHomework/SecondHomeworkTests/Testings.fs
@@ -2,47 +2,34 @@ module SecondHomeworkTests
 
 open NUnit.Framework
 open functions
+open FsCheck
 
 [<Test>]
 let EqualityMapFoldFilter () =
-    let firstList = [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10 ]
-
-    let secondList =
-        [ 11
-          12
-          13
-          14
-          15
-          17
-          19
-          228
-          1337
-          19 ]
-
-    let thirdList = [ 228; 228; 228; 228 ]
-
     let checkOnList list =
-        Assert.AreEqual(mapEven list, filterEven list)
-        Assert.AreEqual(mapEven list, foldEven list)
-        Assert.AreEqual(filterEven list, foldEven list)
-
-    checkOnList firstList
-    checkOnList secondList
-    checkOnList thirdList
+        mapEven list = filterEven list && mapEven list = foldEven list && filterEven list = foldEven list
+    
+    Check.QuickThrowOnFailure(checkOnList)
 
 [<Test>]
 let MapTreeTest () =
     let tree =
-        BinaryTree<float>.Node (2., BinaryTree<float>.Node (3., BinaryTree<float>.Node (4., None, None), None), None)
+        BinaryTree.Node (2., BinaryTree.Node (3., BinaryTree.Node (4., None, None), None), None)
 
     let treeAfterApplyingIterFunction =
-        BinaryTree<float>.Node (3., BinaryTree<float>.Node (4., BinaryTree<float>.Node (5., None, None), None), None)
+        BinaryTree.Node (3., BinaryTree.Node (4., BinaryTree.Node (5., None, None), None), None)
 
     let treeAfterApplyingPow2 =
-        BinaryTree<float>.Node (4., BinaryTree<float>.Node (9., BinaryTree<float>.Node (16., None, None), None), None)
+        BinaryTree.Node (4., BinaryTree.Node (9., BinaryTree.Node (16., None, None), None), None)
+        
+    let nullLeafs = BinaryTree.Node(1337, BinaryTree.None, BinaryTree.None)
+    
+    let nullLeafsAfterApplyingIteration = BinaryTree.Node(228, BinaryTree.None, BinaryTree.None)
 
     Assert.AreEqual(mapBinaryTree (fun a -> a ** 2.) tree, treeAfterApplyingPow2)
     Assert.AreEqual(mapBinaryTree (fun a -> a + 1.) tree, treeAfterApplyingIterFunction)
+    Assert.AreEqual(mapBinaryTree (fun a -> a + 1.) tree, treeAfterApplyingIterFunction)
+    Assert.AreEqual(mapBinaryTree (fun a -> a * 228 / 1337) nullLeafs, nullLeafsAfterApplyingIteration)
 
 
 [<Test>]
@@ -50,7 +37,7 @@ let arithmeticTreeTest () =
     let firstTree = Node(Sum, Leaf 114, Leaf 114)
 
     let secondTree =
-        Node(Multiply, Node(Sum, Leaf 2, Leaf 2), Node(Divide, Leaf 4, Leaf 2))
+        Node(Multiply, Node(Subtract, Leaf 2, Leaf 2), Node(Divide, Leaf 4, Leaf 2))
 
     Assert.AreEqual(228, evaluateExpression firstTree)
-    Assert.AreEqual(8, evaluateExpression secondTree)
+    Assert.AreEqual(0, evaluateExpression secondTree)
