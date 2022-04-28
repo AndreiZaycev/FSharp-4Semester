70b7f70c150d6b7647478c02508f3ce82e0f232a
AndreiZaycev <Mikowftv30@mail.ru>
2022-04-28 23:07:06 +0300

    fix comments

--- PointFree/PointFreeTest/PointFreeTest.fs
+++ PointFree/PointFreeTest/PointFreeTest.fs
@@ -2,11 +2,11 @@ module PointFreeTest
 
 open NUnit.Framework
 open FsCheck
-open Program
+open PointFree
 
 [<Test>]
 let Test() =
-    Check.QuickThrowOnFailure (fun a b -> first a b = third() a b)
+    Check.QuickThrowOnFailure (fun a b -> first a b = third a b)
     Check.QuickThrowOnFailure (fun a b -> first a b = second a b)
-    Check.QuickThrowOnFailure (fun a b -> second a b = third() a b)
+    Check.QuickThrowOnFailure (fun a b -> second a b = third a b)
     
