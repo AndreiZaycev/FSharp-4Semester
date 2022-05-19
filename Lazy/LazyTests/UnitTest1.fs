acd799e40d1651d8d2e605b65fc88f8c144d2caa
AndreiZaycev <Mikowftv30@mail.ru>
2022-05-19 23:12:22 +0300

    lazy done

---
+++ Lazy/LazyTests/UnitTest1.fs
@@ -0,0 +1,47 @@
+module LazyTests
+
+open System.Threading
+open NUnit.Framework
+open Lazy
+open FsUnit
+
+[<Test>]
+let SuperTest() =
+    let string = "idu po raionu jinsi visyat nizko"
+    let foo = fun _ -> string
+    let naiveChecker = LazyFactory.NaiveLazy(foo)
+    let lockFreeChecker = LazyFactory.LockFreeLazy(foo)
+    let concurrentChecker = LazyFactory.ConcurrentLazy(foo)
+    
+    naiveChecker.Get() |> should equal string
+    lockFreeChecker.Get() |> should equal string
+    concurrentChecker.Get() |> should equal string 
+    
+    let currentSecond() = System.DateTime.Now.Second
+    let naive = LazyFactory.NaiveLazy(currentSecond)
+    let lockFree = LazyFactory.LockFreeLazy(currentSecond)
+    let concurrent = LazyFactory.ConcurrentLazy(currentSecond)
+    
+    let fst = naive.Get()
+    Thread.Sleep(1000)
+    let snd = naive.Get()
+    Thread.Sleep(1000)
+    let thrd = lockFree.Get()
+    Thread.Sleep(1000)
+    let frth = lockFree.Get()
+    Thread.Sleep(1000)
+    let ffth = concurrent.Get()
+    Thread.Sleep(1000)
+    let sxth = concurrent.Get()
+    Thread.Sleep(1000)
+    
+    fst |> should equal snd
+    thrd |> should equal frth
+    ffth |> should equal sxth
+    
+    [ for _ in 0 .. 10 do async {
+        Thread.Sleep(500)
+        concurrent.Get() |> should equal ffth
+        lockFree.Get() |> should equal thrd
+    } ] |> Async.Parallel |> Async.RunSynchronously |> ignore
+  
