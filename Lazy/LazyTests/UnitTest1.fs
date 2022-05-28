d1802c0fb93bc4aa83aa9d9469abfbc600e75216
AndreiZaycev <Mikowftv30@mail.ru>
2022-05-28 22:23:50 +0300

    fix smth

--- Lazy/LazyTests/UnitTest1.fs
+++ Lazy/LazyTests/UnitTest1.fs
@@ -1,51 +1,60 @@
 module LazyTests
 
-open System.Threading
 open NUnit.Framework
 open Lazy
 open FsUnit
-let currentSecond() = System.DateTime.Now.Second
-let naive = LazyFactory.NaiveLazy(currentSecond)
-let lockFree = LazyFactory.LockFreeLazy(currentSecond)
-let concurrent = LazyFactory.ConcurrentLazy(currentSecond)
-
-let fst = naive.Get()
-Thread.Sleep(1000)
-let snd = naive.Get()
-Thread.Sleep(1000)
-let thrd = lockFree.Get()
-Thread.Sleep(1000)
-let frth = lockFree.Get()
-Thread.Sleep(1000)
-let ffth = concurrent.Get()
-Thread.Sleep(1000)
-let sxth = concurrent.Get()
-Thread.Sleep(1000)
-    
 
 [<Test>]
-let CheckThatLazyReturnsExpectedString() =
+let NaiveLazyShouldReturnExpectedString() =
     let string = "idu po raionu jinsi visyat nizko"
     let foo = fun _ -> string
-    let naiveChecker = LazyFactory.NaiveLazy(foo)
-    let lockFreeChecker = LazyFactory.LockFreeLazy(foo)
-    let concurrentChecker = LazyFactory.ConcurrentLazy(foo)
-    
+    let naiveChecker = LazyFactory.NaiveLazy(foo)  
     naiveChecker.Get() |> should equal string
+    
+[<Test>]
+let LockFreeLazyShouldReturnExpectedString() =
+    let string = "beb"
+    let foo = fun _ -> string
+    let lockFreeChecker = LazyFactory.LockFreeLazy(foo)
     lockFreeChecker.Get() |> should equal string
+    
+[<Test>]
+let ConcurrentLazyShouldReturnExpectedString() =
+    let string = "opa gagnam style"
+    let foo = fun _ -> string
+    let concurrentChecker = LazyFactory.ConcurrentLazy(foo)
     concurrentChecker.Get() |> should equal string 
        
 [<Test>]    
-let CheckThatLazyAlwaysReturnsFirstCalculation() =   
-    fst |> should equal snd
-    thrd |> should equal frth
-    ffth |> should equal sxth
+let CheckThatNaiveLazyAlwaysReturnsFirstCalculation() =
+    let currentSecond() = System.DateTime.Now.Second 
+    let naive = LazyFactory.NaiveLazy(currentSecond)
+    naive.Get() |> should equal (naive.Get())
+    
+[<Test>]    
+let CheckThatLockFreeLazyAlwaysReturnsFirstCalculation() =
+    let currentSecond() = System.DateTime.Now.Second 
+    let lockFree = LazyFactory.LockFreeLazy(currentSecond)
+    lockFree.Get() |> should equal (lockFree.Get())
+    
+[<Test>]
+let CheckThatConcurrentLazyAlwaysReturnsFirstCalculation() =
+    let currentSecond() = System.DateTime.Now.Second 
+    let concurrent = LazyFactory.ConcurrentLazy(currentSecond)  
+    concurrent.Get() |> should equal (concurrent.Get())
     
 [<Test>]   
-let CheckConcurrency() =
-    [ for _ in 0 .. 10 do async {
-        Thread.Sleep(500)
-        concurrent.Get() |> should equal ffth
-        lockFree.Get() |> should equal thrd
+let CheckConcurrencyInConcurrentLazy() =
+    let random() = System.Random().Next()
+    let concurrent = LazyFactory.ConcurrentLazy(random)
+    [ for _ in 0 .. 100 do async {
+        concurrent.Get() |> should equal (concurrent.Get())
     } ] |> Async.Parallel |> Async.RunSynchronously |> ignore
-  
+    
+[<Test>]   
+let CheckConcurrencyInLockFreeLazy() =
+    let random() = System.Random().Next()
+    let lockFree = LazyFactory.LockFreeLazy(random)
+    [ for _ in 0 .. 100 do async {
+        lockFree.Get() |> should equal (lockFree.Get())
+    } ] |> Async.Parallel |> Async.RunSynchronously |> ignore
