adde56fb640333b18ccd0a04b302366356e08519
AndreiZaycev <Mikowftv30@mail.ru>
2022-06-01 13:17:38 +0300

    some fixes

--- LocalNet/LocalNet/Program.fs
+++ LocalNet/LocalNet/Program.fs
@@ -1,5 +1,7 @@
 module LocalNet
 
+open System.Collections.Generic
+
 /// Implementation of computer's OS 
 type OS =
     | Linux
@@ -11,97 +13,64 @@ type Computer(name: string, os: OS) =
     /// name of the computer
     member this.Name = name 
     /// os of the computer
-    member this.OS = os     
-   
-/// Implementation of virus that can infect computer  
-type Virus(Name: string, Random: unit -> float, Linux: float, Windows: float, MacOS: float, Infected: List<Computer>) =
-    let mutable infected = Infected
-    member this.Infect computer = infected <- computer :: infected
-    member this.GetInfected() = infected
-    member this.Name = Name 
-    member this.Random = Random 
-    member this.Linux = Linux 
-    member this.Windows = Windows 
-    member this.MacOS = MacOS 
-    member this.Infected = Infected
-
-/// Implementation of network
-type Net(computers: List<Computer>, mtx: bool[,], viruses: List<Virus>) =
+    member this.OS = os
+ 
+/// Решил так сделать, чтобы они явно не передавались в Virus  
+let mutable matrix = Array2D.zeroCreate 0 0 
+let mutable computers = Array.empty
     
-    /// One turn of infection
-    member this.Turn() =        
-        let mutable infectedThisTurn = List.empty
-        
-        // Gets the infected computers on the previous turns 
-        let infectedOnPreviousTurns =
-            let arr = Array.zeroCreate computers.Length
-            List.iter (fun (virus: Virus) ->
-                          for computer in virus.GetInfected() do
-                              let index = List.findIndex (fun elem -> elem = computer) computers
-                              arr[index] <- true)
-                      viruses
-            arr
-         
-        // Checks computer's infection    
-        let isNotInfected(computer: Computer) =
-            let index = List.findIndex (fun comp -> comp = computer) computers
-            not (List.exists (fun elem -> elem = computer) infectedThisTurn) && not infectedOnPreviousTurns.[index]
-            
-        let mutable isStable = true
-          
-        // Finds all infected connections to the computer and tries to infect him 
-        let tryToInfectComputer(computer: Computer) =
-            // Index of computer 
-            let index = List.findIndex (fun comp -> comp = computer) computers
-            for i in 0 .. computers.Length - 1 do
-                let com = computers.[i]
-                if com <> computer && mtx[index, i] && infectedOnPreviousTurns.[i]
-                   && not (List.contains com infectedThisTurn)
-                then
-                    // Gets all viruses that can potentially infect our computer 
-                    let virusesCanPotentiallyInfectComputer = 
-                        List.filter (fun (virus: Virus) -> List.contains com (virus.GetInfected())) viruses
-                    let condition (v: Virus) discriminatedUnionNumber =
-                        let virusOSProbability = 
-                            match discriminatedUnionNumber with
-                            | 0 -> v.Linux
-                            | 1 -> v.Windows
-                            | _ -> v.MacOS
-                        let virusRandom = v.Random()
-                        if virusOSProbability > 0 && virusRandom > virusOSProbability then isStable <- false
-                        if virusRandom <= virusOSProbability && not (List.contains computer infectedThisTurn)
-                            then
-                                isStable <- false
-                                v.Infect computer
-                                // современные проблемы требуют современных решений 
-                                infectedThisTurn <-  computer :: infectedThisTurn
-                    List.iter
-                        (fun (virus: Virus) ->
-                            match computer.OS with
-                            | Linux -> condition virus 0 
-                            | Windows -> condition virus 1 
-                            | MacOS -> condition virus 2)
-                        virusesCanPotentiallyInfectComputer
-                           
-        for i in 0 .. computers.Length - 1 do
-            let computer = computers[i] 
-            if isNotInfected(computer) then tryToInfectComputer(computer)          
-            
-        for j in 0 .. computers.Length - 1 do 
-            let message =
-                if infectedOnPreviousTurns.[j] || (List.exists (fun elem -> elem = computers.[j]) infectedThisTurn)
-                then "is infected" else "is not infected"
-            printfn $"%s{computers.[j].Name} %s{message}"  
-        
-        isStable     
+type Virus(firstInfected: Computer[], infectionChance: OS -> float, random: unit -> float) =
+    let infectedComputers = HashSet(firstInfected)
 
-    /// starts infection
-    member this.Start() =
-        let mutable isStable = false
-        let mutable counter = 0
-        while not isStable do
-            counter <- counter + 1
-            printfn $"Turn %A{counter}"
-            isStable <- this.Turn()
-        printfn "Network is stable"
-        counter
+    let getNeighbours computer =
+        let indexOfComputer = Array.findIndex (fun (comp: Computer) -> computer = comp) computers
+        let slice =
+            matrix[indexOfComputer, *]
+            |> Array.mapi (fun i hasConnection -> (i, hasConnection))
+            |> Array.filter snd
+            |> Array.map (fun (i, hasConnection) -> computers.[i])
+        slice 
+    
+
+    let calculateInfectionCandidates lastInfected =
+        lastInfected
+        |> Seq.map getNeighbours  
+        |> Seq.concat
+        |> Seq.distinct
+        |> Seq.filter (not << infectedComputers.Contains)
+        |> Seq.filter (fun x -> infectionChance x.OS > 0)
+
+    let mutable infectionCandidates = calculateInfectionCandidates infectedComputers
+
+    member x.InfectedComputers = infectedComputers
+
+    member x.AbleToInfect = not (Seq.isEmpty infectionCandidates)
+
+    member x.SpreadInfection() =
+        let newInfected = List()
+
+        for computer in infectionCandidates do
+            if random() < infectionChance computer.OS then
+                newInfected.Add(computer)
+
+        infectedComputers.UnionWith(newInfected)
+        infectionCandidates <- calculateInfectionCandidates newInfected
+
+
+/// Implementation of plague inc 
+type PlagueInc(comps: Computer[], mtx: bool[,], viruses: Virus[]) =
+    let rec play viruses counter =
+        printfn $"turn %A{counter}"
+        let viruses = viruses |> Seq.filter (fun (v: Virus) -> v.AbleToInfect)
+        if Seq.isEmpty viruses
+        then counter 
+        else
+            for virus in viruses do
+                virus.SpreadInfection()
+            play viruses (counter + 1)
+        
+    member x.Play() =
+        computers <- comps
+        matrix <- mtx 
+        play viruses 1 
+                          
--- LocalNet/TestLocalNet/LocalNetTest.fs
+++ LocalNet/TestLocalNet/LocalNetTest.fs
@@ -10,11 +10,16 @@ open Foq
 let testWithProbabilities1 () =
     let computer1 = Computer("Computer1", Linux)
     let computer2 = Computer("Computer2", Windows)
-    let virus1 = Virus("ivanMOSKALENKO", System.Random().NextDouble, 1, 1, 1, List.singleton computer1)
-    let virus2 = Virus("ALEXANDERKURDAKOV", System.Random().NextDouble, 1, 1, 1, List.empty)
-    let virus3 = Virus("DEDSEC", System.Random().NextDouble, 1, 1, 1, List.empty)
-    let network = Net([ computer1; computer2; ],  Array2D.init 2 2 (fun _ _ -> true), [ virus1; virus2; virus3 ])
-    let turns = network.Start()
+    let foo os =
+        match os with
+        | Linux -> 1.
+        | Windows -> 1.
+        | MacOS -> 1.
+    let virus = Virus([|computer1|], foo, System.Random().NextDouble)
+    let virus1 = Virus([||], foo, System.Random().NextDouble)
+    let virus2 = Virus([||], foo, System.Random().NextDouble)
+    let network = PlagueInc([| computer1; computer2; |],  Array2D.init 2 2 (fun _ _ -> true), [| virus; virus1; virus2; |])
+    let turns = network.Play()
     turns |> should equal 2
       
 [<Test>]
@@ -22,12 +27,17 @@ let oneMoreTestWithProbabilities1 () =
     let computer1 = Computer("Computer1", Linux)
     let computer2 = Computer("Computer2", Windows)
     let computer3 = Computer("Computer2", MacOS)
-    let virus1 = Virus("ivanMOSKALENKO", System.Random().NextDouble, 1, 1, 1, List.singleton computer1)
-    let virus2 = Virus("ALEXANDERKURDAKOV", System.Random().NextDouble, 1, 1, 1, List.empty)
-    let virus3 = Virus("DEDSEC", System.Random().NextDouble, 1, 1, 1, List.empty)
+    let foo os =
+        match os with
+        | Linux -> 1.
+        | Windows -> 1.
+        | MacOS -> 1.
+    let virus = Virus([|computer1|], foo, System.Random().NextDouble)
+    let virus1 = Virus([||], foo, System.Random().NextDouble)
+    let virus2 = Virus([||], foo, System.Random().NextDouble)
     let arr = [| [| true; true; false |]; [| true; true; true |]; [| false; true; true |] |]
-    let network = Net([computer1; computer2; computer3],  Array2D.init 3 3 (fun i j -> arr[i][j]), [ virus1; virus2; virus3 ])
-    let turns = network.Start()
+    let network = PlagueInc([|computer1; computer2; computer3|],  Array2D.init 3 3 (fun i j -> arr[i][j]), [| virus; virus1; virus2; |])
+    let turns = network.Play()
     turns |> should equal 3
     
 [<Test>]                     
@@ -35,13 +45,18 @@ let determinedRandomTest () =
     let computer1 = Computer("Computer1", Linux)
     let computer2 = Computer("Computer2", Windows)
     let computer3 = Computer("Computer2", MacOS)
-    let foo = fun () -> 0.15
-    let virus1 = Virus("ivanMOSKALENKO", foo, 0.5, 0.5, 0.5, List.singleton computer1)
-    let virus2 = Virus("ALEXANDERKURDAKOV",foo, 0.5, 0.5, 0.5, List.empty)
-    let virus3 = Virus("DEDSEC", foo, 0.5, 0.5, 0.5, List.empty)
+    let foo1 = fun () -> 0.15
+    let foo os =
+        match os with
+        | Linux -> 0.5
+        | Windows -> 0.5
+        | MacOS -> 0.5
+    let virus = Virus([|computer1|], foo, foo1)
+    let virus1 = Virus([||], foo, foo1)
+    let virus2 = Virus([||], foo, foo1)
     let arr = [| [| true; true; false |]; [| true; true; true |]; [| false; true; true |] |]
-    let network = Net([computer1; computer2; computer3],  Array2D.init 3 3 (fun i j -> arr[i][j]), [ virus1; virus2; virus3 ])
-    let turns = network.Start()
+    let network = PlagueInc([|computer1; computer2; computer3|],  Array2D.init 3 3 (fun i j -> arr[i][j]), [| virus; virus1; virus2; |])
+    let turns = network.Play()
     turns |> should equal 3
 
     
@@ -50,11 +65,16 @@ let stableTest () =
     let computer1 = Computer("Computer1", Linux)
     let computer2 = Computer("Computer2", Windows)
     let computer3 = Computer("Computer2", MacOS)
-    let virus1 = Virus("ivanMOSKALENKO", System.Random().NextDouble, 0.5, 0.5, 0.5, List.singleton computer1)
-    let virus2 = Virus("ALEXANDERKURDAKOV",System.Random().NextDouble, 0.5, 0.5, 0.5, List.empty)
-    let virus3 = Virus("DEDSEC", System.Random().NextDouble, 0.5, 0.5, 0.5, List.empty)
-    let network = Net([computer1; computer2; computer3],  Array2D.init 3 3 (fun _ _ -> false), [ virus1; virus2; virus3 ])
-    let turns = network.Start()
+    let foo os =
+        match os with
+        | Linux -> 0.5
+        | Windows -> 0.5
+        | MacOS -> 0.5
+    let virus = Virus([|computer1|], foo, System.Random().NextDouble)
+    let virus1 = Virus([||], foo, System.Random().NextDouble)
+    let virus2 = Virus([||], foo, System.Random().NextDouble)
+    let network = PlagueInc([|computer1; computer2; computer3|],  Array2D.init 3 3 (fun _ _ -> false), [| virus1; virus2; virus |])
+    let turns = network.Play()
     turns |> should equal 1
     
 [<Test>]                     
@@ -62,9 +82,14 @@ let oneMoreStableTest () =
     let computer1 = Computer("Computer1", Linux)
     let computer2 = Computer("Computer2", Windows)
     let computer3 = Computer("Computer2", MacOS)
-    let virus1 = Virus("ivanMOSKALENKO", System.Random().NextDouble, 0, 0, 0, List.singleton computer1)
-    let virus2 = Virus("ALEXANDERKURDAKOV",System.Random().NextDouble, 0, 0, 0, List.empty)
-    let virus3 = Virus("DEDSEC", System.Random().NextDouble, 0, 0, 0, List.empty)
-    let network = Net([computer1; computer2; computer3],  Array2D.init 3 3 (fun _ _ -> false), [ virus1; virus2; virus3 ])
-    let turns = network.Start()
+    let foo os =
+        match os with
+        | Linux -> 0.
+        | Windows -> 0.
+        | MacOS -> 0.
+    let virus = Virus([|computer1|], foo, System.Random().NextDouble)
+    let virus1 = Virus([||], foo, System.Random().NextDouble)
+    let virus2 = Virus([||], foo, System.Random().NextDouble)
+    let network = PlagueInc([|computer1; computer2; computer3|],  Array2D.init 3 3 (fun _ _ -> false), [| virus1; virus2; virus |])
+    let turns = network.Play()
     turns |> should equal 1  
